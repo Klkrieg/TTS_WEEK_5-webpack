e95e2f80a5f0adad899d6b3ba362004bb9fbe0e6
Karson Krieg <klkrieg3@gmail.com>
2021-01-13 21:10:08 -0600

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <textarea></textarea>
+    </div>
+  );
+};
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const htmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebPackPlugin({
+      template: './src/index.html',
+      fileName: './index.html',
+    }),
+  ],
+};
